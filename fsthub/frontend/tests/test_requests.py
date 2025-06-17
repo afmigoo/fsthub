800564c174a4392ec546fb5f86aed86b7f6e7937
Elen Kartina <affenmilchmann@gmail.com>
2025-06-17 20:06:11 +0300

    frontend testing with requests added

--- fsthub/frontend/tests/test_requests.py
+++ fsthub/frontend/tests/test_requests.py
@@ -1,56 +1,59 @@
 """
-Test apps' views through a `django.test.Client` object.
+Test apps' views through real HTTP requests from outside.
+All tests match tests in `test_client.py`, but are done
+from outside with the `reqests` module.
 """
 from dotenv import load_dotenv, find_dotenv
-from django.test import Client
-from unittest import TestCase as UnitTestCase
+from django.test import LiveServerTestCase
 
+from typing import Tuple
 import os
+import requests
 
-class TestPingViews(UnitTestCase):
+def get_html(url: str, headers: dict = None) -> Tuple[int, str]:
+    resp = requests.get(url, headers=headers)
+    return resp.status_code, resp.content.decode(encoding='utf-8')
+
+class TestPingViews(LiveServerTestCase):
     def setUp(self):
         load_dotenv(find_dotenv('.env'))
         self.url_prefix = os.getenv('FSTHUB_PREFIX')
         self.url_prefix = '' if self.url_prefix is None else self.url_prefix
         self.url_prefix = '/' + self.url_prefix
-        self.client = Client()
 
     def test_ping_index(self):
-        href = f'{self.url_prefix}'
-        resp = self.client.get(href)
-        self.assertEqual(resp.status_code, 200, f'{href} - {resp}')
+        url = f'{self.live_server_url}{self.url_prefix}'
+        code, _ = get_html(url)
+        self.assertEqual(code, 200, f'{url} - {code}')
     def test_ping_browse(self):
-        href = f'{self.url_prefix}browse'
-        resp = self.client.get(href)
-        self.assertEqual(resp.status_code, 200, f'{href} - {resp}')
+        url = f'{self.live_server_url}{self.url_prefix}browse'
+        code, _ = get_html(url)
+        self.assertEqual(code, 200, f'{url} - {code}')
     def test_ping_about(self):
-        href = f'{self.url_prefix}about'
-        resp = self.client.get(href)
-        self.assertEqual(resp.status_code, 200, f'{href} - {resp}')
+        url = f'{self.live_server_url}{self.url_prefix}about'
+        code, _ = get_html(url)
+        self.assertEqual(code, 200, f'{url} - {code}')
 
-class TestLangLocale(UnitTestCase):
+class TestLangLocale(LiveServerTestCase):
     def setUp(self):
         load_dotenv(find_dotenv('.env'))
         self.url_prefix = os.getenv('FSTHUB_PREFIX')
         self.url_prefix = '' if self.url_prefix is None else self.url_prefix
         self.url_prefix = '/' + self.url_prefix
-        self.client = Client()
 
     def test_ping_index_en(self):
-        href = f'{self.url_prefix}'
-        resp = self.client.get(href, headers={
+        url = f'{self.live_server_url}{self.url_prefix}'
+        _, html = get_html(url, headers={
             'Accept-Language': 'en-US,en;q=0.5'
         })
-        html = resp.content.decode()
         self.assertIn(f'<a href="{self.url_prefix}">Home</a>', html)
         self.assertIn(f'<a href="{self.url_prefix}browse">FST catalog</a>', html)
         self.assertIn(f'<a href="{self.url_prefix}about">About</a>', html)
     def test_ping_index_ru(self):
-        href = f'{self.url_prefix}'
-        resp = self.client.get(href, headers={
+        url = f'{self.live_server_url}{self.url_prefix}'
+        _, html = get_html(url, headers={
             'Accept-Language': 'ru-RU,ru;q=0.5'
         })
-        html = resp.content.decode()
         self.assertIn(f'<a href="{self.url_prefix}">Главная</a>', html)
         self.assertIn(f'<a href="{self.url_prefix}browse">Каталог FST</a>', html)
         self.assertIn(f'<a href="{self.url_prefix}about">О сайте</a>', html)
