c20aadb2f437a7008e61dd12d0e897a90785ae69
Elen Kartina <affenmilchmann@gmail.com>
2025-06-22 00:09:01 +0300

    project reader random access test

--- fsthub/project_reader/tests.py
+++ fsthub/project_reader/tests.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from typing import List
 from random import getrandbits
 from time import sleep
+import random
 import shutil
 
 from . import ProjectReader as PR
@@ -126,6 +127,7 @@ class TestPRViews(TestCase):
         self.assertEqual(t_count, 0)
 
     def test_all_exist(self):
+        sleep(0.1)
         Np = 3
         Nfst = 3
 
@@ -155,3 +157,99 @@ class TestPRViews(TestCase):
 
         self.assertFalse(PR.fst_exists('some/thing'))
         self.assertFalse(PR.project_exists('some'))
+
+class TestPRRandom(TestCase):
+    temp_root = Path(__file__).parent / 'tmp'
+    @classmethod
+    def setUpClass(cls):
+        # Create a temp dir structure
+        cls.temp_root.mkdir()
+        PR.root = cls.temp_root
+        PR.update_interval = 0.5  # Short interval for faster testing
+        
+        # Create test projects/files
+        cls.files = {
+            p: [f"file_{i}.hfst" for i in range(random.randint(1, 15))]
+            for p in [f"Project_{i}" for i in range(31)]
+        }
+        
+        for project, files in cls.files.items():
+            (cls.temp_root / project).mkdir()
+            for f in files:
+                (cls.temp_root / project / f).touch()
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(cls.temp_root)
+
+    def test_random_operations(self):
+        operations = [
+            self._get_projects,
+            self._get_all_fsts,
+            self._get_project_fsts,
+            self._project_exists,
+            self._fst_exists,
+            self._add_project,
+            self._del_project,
+            self._add_fst,
+            self._del_fst,
+        ]
+        
+        for _ in range(50):  # Adjust count as needed
+            op = random.choice(operations)
+            op()
+            sleep(0.1)  # Allow cache updates occasionally
+
+    def _get_projects(self):
+        result = list(PR.get_projects())
+        self.assertEqual(set(result), set(self.files.keys()))
+        
+    def _get_all_fsts(self):
+        all_files = [
+            f"{p}/{f}" for p in self.files.keys()
+            for f in self.files[p]
+        ]
+        result = list(PR.get_all_fsts())
+        self.assertEqual(set(result), set(all_files))
+        
+    def _get_project_fsts(self):
+        project = random.choice(list(self.files.keys()))
+        result = list(PR.get_fsts(project))
+        self.assertEqual(set(result), set(f'{project}/{f}' for f in self.files[project]))
+        
+    def _project_exists(self):
+        project = random.choice(list(self.files.keys()) + ["nonexistent"])
+        expected = project in self.files.keys()
+        self.assertEqual(PR.project_exists(project), expected)
+        
+    def _fst_exists(self):
+        project = random.choice(list(self.files.keys()))
+        fname = random.choice(self.files[project] + ["ghost.hfst"])
+        path = f"{project}/{fname}"
+        expected = fname in self.files[project]
+        self.assertEqual(PR.fst_exists(path), expected)
+
+    def _add_project(self):
+        new_proj = f'RandProj{random.randint(1, 10_000_000)}'
+        before_update = set(PR.get_projects())
+        self.assertSetEqual(before_update, set(str(d.relative_to(self.temp_root)) 
+                                               for d in self.temp_root.iterdir() if d.is_dir()))
+        self.assertSetEqual(before_update, set(self.files.keys()))
+
+        (self.temp_root / new_proj).mkdir(exist_ok=True)
+        if not new_proj in self.files.keys():
+            self.files[new_proj] = []
+        sleep(PR.update_interval)
+        after_update = set(PR.get_projects())
+        self.assertSetEqual(after_update, set([str(d.relative_to(self.temp_root)) 
+                                               for d in self.temp_root.iterdir() if d.is_dir()]))
+        self.assertSetEqual(after_update, set(self.files.keys()))
+
+    def _del_project(self):
+        pass
+
+    def _add_fst(self):
+        pass
+
+    def _del_fst(self):
+        pass
