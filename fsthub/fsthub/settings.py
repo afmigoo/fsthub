f05e1f0bc111679a08b0d6789d2f7ee21e2de233
Elen Kartina <affenmilchmann@gmail.com>
2025-06-24 22:02:44 +0300

    CD fix: using venv to collect static

--- fsthub/fsthub/settings.py
+++ fsthub/fsthub/settings.py
@@ -17,20 +17,14 @@ import os
 from django.utils.translation import gettext_lazy as _
 
 load_dotenv(find_dotenv('settings.env'))
+load_dotenv(find_dotenv('secret.env'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j)uxw*-s+oyyp&-v-gj_fh%%g%$qbtgd6^n)tn#lbl)^-tlyse'
-
-# SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 DEBUG = os.getenv('DJANGO_DEBUG', 'FALSE') == 'TRUE'
-
 ALLOWED_HOSTS = [os.getenv('DJANGO_HOST', 'localhost')]
 
 
